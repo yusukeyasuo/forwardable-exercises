5d19e39a3b96b8d22e842b5d1ab56956ec43a268
Yusuke Yasuo <yasuo.y@dely.jp>
2019-06-20 20:45:09 +0900

    refactor

--- implement_queue/my_queue.rb
+++ implement_queue/my_queue.rb
@@ -9,8 +9,5 @@ class MyQueue
 
   def_delegator :@queue, :push, :enqueue
   def_delegator :@queue, :shift, :dequeue
-  def_delegator :@queue, :first, :first
-  def_delegator :@queue, :last, :last
-  def_delegator :@queue, :clear, :clear
-  def_delegator :@queue, :size, :size
+  def_delegators :@queue, :first, :last, :clear, :size
 end
